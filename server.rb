75f253da8e346a9d3ead5f2cb1d8606f7b5fab57
Billy Chen <billy.chen1206@gmail.com>
2019-06-27 13:17:54 -0400

    some pictures added

--- server.rb
+++ server.rb
@@ -15,3 +15,31 @@ muffin1 = Muffin.new("Peanut Butter Nutella", "Savory Nutella swirl with creamy 
 get "/" do
   erb :home
 end
+
+get "/muffins" do
+  erb :muffins
+end
+
+get "/cookies" do
+  erb :cookies
+end
+
+get "/cupcakes" do
+  erb :cupcakes
+end
+
+get "/cakes" do
+  erb :cakes
+end
+
+get "/crepes" do
+  erb :crepes
+end
+
+get "/events" do
+  erb :events
+end
+
+get "/partners" do
+  erb :partners
+end
