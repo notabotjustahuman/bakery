2895aaa4af6bbaffb87d9d549f76a75f866a8883
unknown <billy.chen1206@gmail.com>
2019-06-27 00:38:43 -0400

    muffin class created

--- server.rb
+++ server.rb
@@ -1,6 +1,17 @@
 require 'sinatra'
 require 'httparty'
 
+class Muffin
+  attr_accessor :name, :description, :price
+  def initialize(name,description,price)
+    @name = name
+    @description = description
+    @price = price
+  end
+end
+
+muffin1 = Muffin.new("Peanut Butter Nutella", "Savory Nutella swirl with creamy peanut butter", "Price: $3.95")
+
 get "/" do
   erb :home
 end
