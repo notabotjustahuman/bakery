71e7508864d9be8a82eb4fa23f2554671feac4b9
unknown <billy.chen1206@gmail.com>
2019-06-30 23:03:38 -0400

    newsletter updated, improvements to other sheets

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     document.getElementById("topButton").style.display = "inline-block";
   } else {
     document.getElementById("topButton").style.display = "none";
